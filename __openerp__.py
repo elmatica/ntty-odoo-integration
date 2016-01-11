6149f774303fc5a4417204d2a07469052c1f5be4
morrillo <gustavo.orrillo@moldeo.coop>
2016-01-11 11:17:15 -0300

    Added manifest file

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'NTTY',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['base','product','product_brand'],
+    'data': [
+	'views/ntty_view.xml'
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
